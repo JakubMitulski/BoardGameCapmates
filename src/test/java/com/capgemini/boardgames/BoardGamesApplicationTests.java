ca227fed7af134c9934f466e87af593c04f1ce81
Jakub <jakub.mitulski@gmail.com>
2018-07-22 13:48:36 +0200

    Init commit

---
+++ src/test/java/com/capgemini/boardgames/BoardGamesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.boardgames;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardGamesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
