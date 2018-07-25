4d2b8e04c6facc847babfb83c184e432e6ca1d08
JakubMitulski <jakub.mitulski@gmail.com>
2018-07-25 11:32:47 +0200

    Added playability comparator

--- src/main/java/com/capgemini/boardgames/model/playability/PlayabilityComparator.java
+++ src/main/java/com/capgemini/boardgames/model/playability/PlayabilityComparator.java
@@ -1,9 +1,19 @@
 package com.capgemini.boardgames.model.playability;
 
+import java.time.LocalTime;
+
 public class PlayabilityComparator {
 
     public boolean compare(Playability firstPlayability, Playability secondPlayability){
 
+        LocalTime firstPlayerStartTime = firstPlayability.getStartTime();
+        LocalTime firstPlayerEndTime = firstPlayability.getEndTime();
+        LocalTime secondPlayerStartTime = secondPlayability.getStartTime();
+        LocalTime secondPlayerEndTime = secondPlayability.getEndTime();
+
+        if (firstPlayerStartTime.compareTo(secondPlayerEndTime) < 0){
+            return firstPlayerEndTime.compareTo(secondPlayerStartTime) > 0;
+        }
         return false;
     }
 }
